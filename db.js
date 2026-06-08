64b033b35791c1c6a528b28e5a7e0baeb5af9d80
Qurat UL Ain <quratullainsabir@gmail.com>
2026-06-08 23:12:13 +0500

    Remove node-fetch in favor of global fetch to fix Vercel deploy

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
-const fetch = require('node-fetch');
 // Path to local fallback JSON file
 const LOCAL_DATA_PATH = path.join(__dirname, 'data.json');
 
