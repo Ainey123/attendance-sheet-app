ba7fadec6a890796c0df61cc01f21c4e6d9f1dd5
Qurat UL Ain <quratullainsabir@gmail.com>
2026-09-08 10:14:33 +0500

    Add global DOMMatrix, Path2D, and ImageData polyfills for Node.js/Vercel serverless environment

--- server.js
+++ server.js
@@ -1,3 +1,49 @@
+// Polyfill DOMMatrix, Path2D, and ImageData for Node.js environment
+if (typeof globalThis.DOMMatrix === 'undefined') {
+  class DOMMatrix {
+    constructor(init) {
+      this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0;
+      this.m11 = 1; this.m12 = 0; this.m13 = 0; this.m14 = 0;
+      this.m21 = 0; this.m22 = 1; this.m23 = 0; this.m24 = 0;
+      this.m31 = 0; this.m32 = 0; this.m33 = 1; this.m34 = 0;
+      this.m41 = 0; this.m42 = 0; this.m43 = 0; this.m44 = 1;
+      this.is2D = true;
+      this.isIdentity = true;
+      if (Array.isArray(init)) {
+        if (init.length === 6) {
+          this.a = this.m11 = init[0]; this.b = this.m12 = init[1];
+          this.c = this.m21 = init[2]; this.d = this.m22 = init[3];
+          this.e = this.m41 = init[4]; this.f = this.m42 = init[5];
+        } else if (init.length === 16) {
+          this.m11 = this.a = init[0]; this.m12 = this.b = init[1]; this.m13 = init[2]; this.m14 = init[3];
+          this.m21 = this.c = init[4]; this.m22 = this.d = init[5]; this.m23 = init[6]; this.m24 = init[7];
+          this.m31 = init[8]; this.m32 = init[9]; this.m33 = init[10]; this.m34 = init[11];
+          this.m41 = this.e = init[12]; this.m42 = this.f = init[13]; this.m43 = init[14]; this.m44 = init[15];
+          this.is2D = false;
+        }
+      }
+    }
+    multiply() { return new DOMMatrix(); }
+    translate() { return new DOMMatrix(); }
+    scale() { return new DOMMatrix(); }
+    rotate() { return new DOMMatrix(); }
+    transformPoint(p) { return p || { x: 0, y: 0, z: 0, w: 1 }; }
+    inverse() { return new DOMMatrix(); }
+  }
+  globalThis.DOMMatrix = DOMMatrix;
+  if (typeof global !== 'undefined') global.DOMMatrix = DOMMatrix;
+}
+if (typeof globalThis.Path2D === 'undefined') {
+  class Path2D { constructor() {} addPath() {} closePath() {} moveTo() {} lineTo() {} bezierCurveTo() {} quadraticCurveTo() {} arc() {} arcTo() {} ellipse() {} rect() {} }
+  globalThis.Path2D = Path2D;
+  if (typeof global !== 'undefined') global.Path2D = Path2D;
+}
+if (typeof globalThis.ImageData === 'undefined') {
+  class ImageData { constructor(w, h) { this.width = w || 1; this.height = h || 1; this.data = new Uint8ClampedArray((this.width * this.height) * 4); } }
+  globalThis.ImageData = ImageData;
+  if (typeof global !== 'undefined') global.ImageData = ImageData;
+}
+
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
